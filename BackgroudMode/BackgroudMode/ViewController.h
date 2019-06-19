ca1fda43f1b9a9ca06be935bf733b7732808db98
liqiang <565384027@qq.com>
2019-06-19 18:40:53 +0800

    finish ~

---
+++ BackgroudMode/BackgroudMode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BackgroudMode
+//
+//  Created by ADMIN on 2019/6/19.
+//  Copyright © 2019 ADMIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BackgroudMode/BackgroudMode/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BackgroudMode
+//
+//  Created by ADMIN on 2019/6/19.
+//  Copyright © 2019 ADMIN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
